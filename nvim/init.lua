01c242af8aca46fe5dc42a9f7bd6418b6f12d6df
pavzari <92022046+pavzari@users.noreply.github.com>
2024-08-27 22:39:59 +0100

    require in statusline config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,7 @@
 require("options")
 require("keymaps")
 require("autocmds")
+require("statusline")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
